b6c7a4a4c5db66aad2f0815fcc153a2767414788
Fredrik Wollsén <fredrik@neam.se>
2013-11-21 00:28:18 +0100

    Only setting the attribute if it is null, so that one can specify the field manually before hitting insert()/save()

--- OwnerBehavior.php
+++ OwnerBehavior.php
@@ -1,13 +1,22 @@
 <?php
 /**
-// Owner Behavior by thyseus <thyseus@gmail.com>
-
-// Assuming a dataset is "owned" by a user, we need to set the id
-// of the current logged in user when saving the dataset automatically.
- * Class OwnerBehavior
+ * Behavior to automate setting the “owned by user" attribute to
+ * the currently logged in user id.
+ *
+ * Based on OwnerBehavior by thyseus
+ * <thyseus@gmail.com>, however
+ * this behavior will only set the attribute if it is null, so that
+ * one can specify the field manually before hitting insert()/save()
+ *
+ * @license MIT
+ * @author thyseus
+ * <thyseus@gmail.com>
+ * @author Fredrik Wollsén
+ * <fredrik@neam.se>
  */
 class OwnerBehavior extends CActiveRecordBehavior
 {
+
     /**
      * The field that stores the pk of the owner
      */
@@ -17,7 +26,9 @@ class OwnerBehavior extends CActiveRecordBehavior
     {
         if (isset($this->owner->tableSchema->columns[$this->ownerColumn])) {
             if ($this->owner->isNewRecord) {
-                $this->owner->{$this->ownerColumn} = Yii::app()->user->id;
+                if (is_null($this->owner->{$this->ownerColumn})) {
+                    $this->owner->{$this->ownerColumn} = Yii::app()->user->id;
+                }
             }
         }
 
